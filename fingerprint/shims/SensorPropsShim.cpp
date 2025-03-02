38e2f23704702c0de73c8fda864b0dd0fd569c26
inferno0230 <mail@inferno0230.in>
2025-03-02 21:28:15 +0530

    fingerprint: Let SensorProps::sensorLocations be configurable
    Oplus fingerprint HAL is setting out-of-bounds FOD location on the
    OnePlus Nord CE4.
    
    OP5D3FL1:/ # getprop | grep persist.vendor.fingerprint.optical.sensorlocation
    [persist.vendor.fingerprint.optical.sensorlocation]: [620::2529]
    
    The physical display dimens is 1080x2400.
    
    Change-Id: I558c46e78b9365b273bef27a3e79a8fd6e47c1dd

--- fingerprint/shims/SensorPropsShim.cpp
+++ fingerprint/shims/SensorPropsShim.cpp
@@ -14,6 +14,15 @@
 
 #include <dlfcn.h>
 
+// Display info headers
+#include <gui/SurfaceComposerClient.h>
+#include <ui/DisplayInfo.h>
+
+using android::DisplayInfo;
+using android::IBinder;
+using android::sp;
+using android::status_t;
+using android::SurfaceComposerClient;
 using android::base::GetProperty;
 using android::base::ParseInt;
 using android::base::Tokenize;
@@ -22,6 +31,36 @@ using aidl::android::hardware::biometrics::fingerprint::FingerprintSensorType;
 using aidl::android::hardware::biometrics::fingerprint::SensorProps;
 
 namespace {
+
+void getScreenDimensions(int32_t* width, int32_t* height) {
+    sp<IBinder> display = SurfaceComposerClient::getInternalDisplayToken();
+    if (display == nullptr) {
+        LOG(ERROR) << "Failed to get display token";
+        return;
+    }
+
+    DisplayInfo info;
+    status_t status = SurfaceComposerClient::getDisplayInfo(display, &info);
+    if (status) {
+        LOG(ERROR) << "Failed to get display info: " << status;
+        return;
+    }
+
+    *width = info.w;
+    *height = info.h;
+
+    if (info.orientation == DISPLAY_ORIENTATION_90 || info.orientation == DISPLAY_ORIENTATION_270) {
+        std::swap(*width, *height);
+    }
+
+    LOG(DEBUG) << "Display info: width=" << *width << ", height=" << *height
+               << ", orientation=" << info.orientation;
+}
+
+bool isWithinBounds(int32_t x, int32_t y, int32_t width, int32_t height) {
+    return (x >= 0 && x <= width && y >= 0 && y <= height);
+}
+
 SensorProps SensorPropsInit(SensorProps props) {
     auto type = GetProperty("persist.vendor.fingerprint.sensor_type", "");
     if (!type.empty()) {
@@ -37,29 +76,64 @@ SensorProps SensorPropsInit(SensorProps props) {
             props.sensorType = FingerprintSensorType::HOME_BUTTON;
     }
 
+    int32_t width = 0, height = 0;
+    getScreenDimensions(&width, &height);
+
+    LOG(DEBUG) << "Screen dimensions: " << width << "x" << height;
+
+    bool valid_location = false;
     auto loc_prop = GetProperty("persist.vendor.fingerprint.optical.sensorlocation", "");
     if (!loc_prop.empty()) {
         auto loc = Tokenize(loc_prop, ":");
-        bool loc_parsed = false;
         if (loc.size() >= 2) {
             int32_t x, y;
-            loc_parsed = ParseInt(loc[0], &x) && ParseInt(loc[1], &y);
-            if (loc_parsed) {
-                props.sensorLocations[0].sensorLocationX = x;
-                props.sensorLocations[0].sensorLocationY = y;
+            if (ParseInt(loc[0], &x) && ParseInt(loc[1], &y)) {
+                // Check if coordinates are within screen bounds
+                if (isWithinBounds(x, y, width, height)) {
+                    props.sensorLocations[0].sensorLocationX = x;
+                    props.sensorLocations[0].sensorLocationY = y;
+                    valid_location = true;
+                    LOG(DEBUG) << "Using direct sensor location: " << x << ":" << y;
+                } else {
+                    LOG(WARNING) << "Sensor location out of bounds: " << x << ":" << y
+                                 << " (screen: " << width << "x" << height << ")";
+                }
+            } else {
+                LOG(WARNING) << "Invalid sensor location format: " << loc_prop;
             }
         }
-        LOG_IF(WARNING, !loc_parsed) << "Invalid sensor location input (x::y): " << loc_prop;
+    }
+
+    if (!valid_location) {
+        props.sensorLocations[0].sensorLocationX = width / 2;
+
+        auto iconlocation = GetProperty("persist.vendor.fingerprint.optical.iconlocation", "");
+        int32_t iconY = 0;
+        if (!iconlocation.empty() && ParseInt(iconlocation, &iconY)) {
+            props.sensorLocations[0].sensorLocationY = height - iconY;
+            LOG(DEBUG) << "Calculated Y location: height (" << height << ") - iconlocation ("
+                       << iconY << ") = " << props.sensorLocations[0].sensorLocationY;
+        } else {
+            props.sensorLocations[0].sensorLocationY = height * 3 / 4;  // 75% down the screen
+            LOG(WARNING) << "No iconlocation property, using default Y location: "
+                         << props.sensorLocations[0].sensorLocationY;
+        }
     }
 
     auto size = GetProperty("persist.vendor.fingerprint.optical.iconsize", "");
     if (!size.empty()) {
         if (ParseInt(size, &props.sensorLocations[0].sensorRadius)) {
             props.sensorLocations[0].sensorRadius /= 2;
+            LOG(DEBUG) << "Setting radius to " << props.sensorLocations[0].sensorRadius
+                       << " (half of iconsize: " << size << ")";
         } else {
             LOG(WARNING) << "Invalid sensor size input: " << size;
         }
     }
+
+    LOG(DEBUG) << "Final sensor properties: location=(" << props.sensorLocations[0].sensorLocationX
+               << ", " << props.sensorLocations[0].sensorLocationY
+               << "), radius=" << props.sensorLocations[0].sensorRadius;
 
     return props;
 }
